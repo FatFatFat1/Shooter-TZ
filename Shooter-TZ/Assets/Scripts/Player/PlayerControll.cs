500c9e7308737bc4bfd949404be613333b6dd672
FatFatFat1 <48385210+FatFatFat1@users.noreply.github.com>
2022-09-15 17:58:07 +0700

    [add] Shot

--- Shooter-TZ/Assets/Scripts/Player/PlayerControll.cs
+++ Shooter-TZ/Assets/Scripts/Player/PlayerControll.cs
@@ -8,11 +8,13 @@ public class PlayerControll : MonoBehaviour
     private PlayerController _pControll;
     private Rigidbody2D _rb2;
     [SerializeField] private float speed;
+    [SerializeField] private GameObject _bullet;
 
     private void Awake()
     {
         _pControll = new PlayerController();
         _rb2 = GetComponent<Rigidbody2D>();
+        _pControll.Player.Shot.started += _ => Shot();
     }
     private void Update()
     {
@@ -30,5 +32,9 @@ public class PlayerControll : MonoBehaviour
     void Move(Vector2 dir)
     {
         _rb2.velocity = dir * Time.deltaTime * speed;
+    }
+    public void Shot()
+    {
+        Instantiate(_bullet, transform.position + transform.right, transform.rotation);
     }
 }
